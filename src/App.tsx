dd27dd224e7fc56a7fc6c907facda8cca574f1f4
Josh Laidlaw <josh.laidlaw@gmail.com>
2025-03-15 16:50:26 -0700

    Add useEffect to test CORS

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,40 @@
+import { useState, useEffect } from 'react'
 import './App.css'
 
 export default function App() {
+
+  const [loading, isLoading] = useState(true)
+  const [pokemon, setPokemon] = useState([])
+  const [total, setTotalRecords] = useState(0)
+
+  useEffect(() => {
+    const controller = new AbortController();
+    fetch(`https://pokeapi.co/api/v2/pokemon/`, {
+      signal: controller.signal,
+    })
+    .then((res) => res.json())
+    .then((data) => {
+      const {count, results, next, prev} = data
+      isLoading(false)
+      setTotalRecords(count)
+      setPokemon(results)
+    })
+    
+    return () => {
+      controller.abort();
+    }
+  }, [])
+  
+
+
   return (
     <main>
-      <h1>Pokédex</h1>
+      {loading && <div>Loading</div>}
+      {pokemon && pokemon.map((pokemon: {name: string}) => (
+          <div key={pokemon.name}>
+            {pokemon.name}
+          </div>
+        ))}
     </main>
   )
 }
--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,9 @@ import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
+    <header>
+      <h1>Pokédex</h1>
+    </header>
     <App />
   </React.StrictMode>
 )
