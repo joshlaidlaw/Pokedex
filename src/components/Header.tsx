682faa188bbf44678fd4a6d1163d82fa726dfe0d
Josh Laidlaw <josh.laidlaw@gmail.com>
2025-03-17 15:55:45 -0700

    Update logo path

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,7 @@ export default function Header() {
     <header className="bg-zinc-100 px-8 py-4 border-b-1 border-slate-300"> 
     <Link to="/">
       <h1 className='text-3xl font-bold '>
-        <img className="h-8" src="/pokémon_logo.svg" alt="Pokémon Logo"/>
+        <img className="h-8" src="pokémon_logo.svg" alt="Pokémon Logo"/>
       </h1>
     </Link>
   </header>
